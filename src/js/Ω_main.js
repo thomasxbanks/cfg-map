43ce98a5e623b56e98917905efbf4228b51fe0aa
thomasxbanks <thomas.banks@wearecube3.com>
2018-03-07 11:39:45 +0000

    BUG - fixed undeclared variable error

--- src/js/Ω_main.js
+++ src/js/Ω_main.js
@@ -2,7 +2,6 @@
 
 window.onload = () => {
   console.log('browser js loaded');
-  console.log('Page Object in main.js', page);
   getBrowserDimensions();
   let masthead = document.getElementById('masthead');
   let mastheadHeight = getComputedHeight(masthead);
